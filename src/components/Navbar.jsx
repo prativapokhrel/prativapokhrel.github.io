77fd714f5d5b092ebcbee8c0ed0d8891a8bde7b9
Prativa <prativa.pokhrel95@gmail.com>
2023-03-28 15:11:00 -0400

    update website title and icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {FaBars, FaTimes} from "react-icons/fa";
 import {Link} from "react-scroll";
-import { HiOutlineMail} from 'react-icons/hi'; 
+// import { HiOutlineMail} from 'react-icons/hi'; 
 import {HiDocumentDownload} from 'react-icons/hi'; 
 
 
